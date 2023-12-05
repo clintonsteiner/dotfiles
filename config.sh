4c80c2efc990a2b9bf397e554c3ac0411978acb6
CJ Steiner <clintonsteiner@gmail.com>
2023-12-05 16:30:32 +0000

    add in ps1

--- config.sh
+++ config.sh
@@ -4,6 +4,7 @@ export CC=clag
 export CXX=clang++
 export PYTHONBREAKPOINT='IPython.core.debugger.set_trace'
 export LOGLEVEL='debug'
+export PS1='\[\e[0;32m\][ $? \[\e[0;33m\]\t \[\e[0;32m\]\u \[\e[0;33m\]\w \[\e[0;32m\]] \$ \[\e[0m\]'
 
 source ./git-completion.bash
 
