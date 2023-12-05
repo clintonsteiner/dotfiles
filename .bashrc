2652d28f9b71170cad7b15d2b7c37935cb182400
CJ Steiner <clintonsteiner@gmail.com>
2023-12-05 14:59:58 +0000

    Final touches to config changes

---
+++ .bashrc
@@ -0,0 +1,10 @@
+if [[ -f ./config.sh ]]; then
+	 . ./config.sh
+fi
+
+if [[ -f ./functions.sh ]]; then
+	 . ./functions.sh
+fi
+if [[ -f ./aliases.sh ]]; then
+	 . ./aliases.sh
+fi
--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ export CXX=clang++
 export PYTHONBREAKPOINT='IPython.core.debugger.set_trace'
 export LOGLEVEL='debug'
 
-source ./.git-completion.bash
+source ./git-completion.bash
 
 if [ -f ./pytest_complete ]; then
      . ./pytest_complete || true
